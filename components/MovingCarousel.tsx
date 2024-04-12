f3b20bee38f4281c359e20179cad32d4ba46e546
divyendra <betudst9107@gmail.com>
2024-04-13 02:01:24 +0530

    MovingCorousel completed

--- components/MovingCarousel.tsx
+++ components/MovingCarousel.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { InfiniteMovingCards } from './ui/infinite-moving-cards'
+import Commands from '../public/Commands.png'
+import Image from 'next/image'
 
 const MovingCarousel = () => {
+    const corouselItems = [
+        {
+            name: 'Create block',
+            logo: ""
+        },
+        {
+            name: 'Invite to create',
+            logo: ""
+        },
+        {
+            name: 'Change theme: Dark',
+            logo: ""
+        },
+        {
+            name: 'Go to board',
+            logo: ""
+        },
+        {
+            name: 'Copy board link',
+            logo: ""
+        },
+        {
+            name: 'Contact support...',
+            logo: ""
+        },
+    ]
   return (
     <div className=' sm:w-[1156px] h-[472px] pt-[64px] pb-12 mb-5 bg-background-color rounded-[20px] flex flex-col items-center ' >
         <h3 className=' text-4xl text-heading-black  '>Every action at your fingertips</h3>
         <p className=' text-sm mt-2 text-text-black text-center '> Stay flow and improve your workflow with our command bar, providign <br/> easy access to all features through just a few keystrokes.</p>
-        <div className='w-full h-[176px] pt-10 pb-12 '></div>
+        <div className='w-full h-[176px] pt-10 pb-12 '>
+            <InfiniteMovingCards items={corouselItems} pauseOnHover={false}   direction='right' />
+            <InfiniteMovingCards items={corouselItems} pauseOnHover={false} direction='left' />'
+        </div>
+        <Image src={Commands} alt='commands' width={467} height={110}/>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,9 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -19,6 +25,17 @@ const config: Config = {
       'black-background': '#201e1d',
     },
     extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -26,6 +43,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+
 export default config;
